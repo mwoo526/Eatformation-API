81ca5e33d5da94fe0b5a9e527b70a09ecfe55b72
Haemin Moon <hm.moon@itda.io>
2018-10-16 01:59:00 +0900

    Add tracking API & Interface DynamoDB & Change xml parser

--- app.js
+++ app.js
@@ -1,29 +1,63 @@
 require('./modules/lib/ff');
 Object.assign(global, ff);
 
-const { getPigButcheryInfoDtl } = require('./app/pig_api/pig_api');
-const pigApiInfo = require('./app/pig_api/pig_api_info');
-
 const ApiBuilder = require('claudia-api-builder');
 const api = new ApiBuilder();
+const aws = require('aws-sdk');
+const dynamoDB = new aws.DynamoDB.DocumentClient();
 
-api.get(`${pigApiInfo.pigButcheryInfoDtl}`, () => {
+const {
+    getPigTraceability, getPigButchery,
+    getPigGrade, getPigPartGrade,
+    getFarmDetailInfo, getFarmTotalInfo } = require('./app/api/tracking_api');
+
+const trackingApiInfo = require('./app/api/config/tracking_api_info');
+
+api.get(`${trackingApiInfo.pig_traceability}`, () => {
     'use strict';
     return go(
-        getPigButcheryInfoDtl(),
-        a => `<div>a</div>`);
+        getPigTraceability());
 });
 
-go(getPigButcheryInfoDtl(), log);
-
-api.get('/test', () => {
+api.get(`${trackingApiInfo.pig_butchery}`, () => {
     'use strict';
-    return 'hello Test';
+    return go(
+        getPigButchery(), a => JSON.stringify(a));
 });
 
-api.post('/hello', (request) => {
+api.get(`${trackingApiInfo.pig_grade}`, () => {
     'use strict';
-    return request;
+    return go(
+        getPigGrade(), a => JSON.stringify(a));
 });
+
+api.get(`${trackingApiInfo.pig_part_grade}`, () => {
+    'use strict';
+    return go(
+        getPigPartGrade());
+});
+
+api.get(`${trackingApiInfo.farm_detail_info}`, () => {
+    'use strict';
+    return go(
+        getFarmDetailInfo());
+});
+
+api.get(`${trackingApiInfo.farm_total_info}`, () => {
+    'use strict';
+    return go(
+        getFarmTotalInfo());
+});
+
+// go(getPigButchery(), a => JSON.stringify(a), log);
+// go(getFarmTotalInfo(), a => JSON.stringify(a), log);
+// go(getFarmDetailInfo(), a => JSON.stringify(a), log);
+// go(getPigTraceability(), a => JSON.stringify(a), log);
+// go(getPigGrade(), a => JSON.stringify(a), log);
+// go(getPigPartGrade(), a => JSON.stringify(a), log);
 
 module.exports = api;
+
+
+
+api.addPostDeployConfig('tableName', 'DynamoDB Table Name:', 'configure-db');
---
+++ app/api/tracking_api.js
@@ -0,0 +1,105 @@
+const request = require('request');
+const parser = require('fast-xml-parser');
+
+const trackingApiInfo = require('./config/tracking_api_info');
+
+// https://zfhwg0i742.execute-api.ap-northeast-2.amazonaws.com/latest/rest/pigTraceabilityInfo.xml
+const getPigTraceability = () => {
+    const endPoint = trackingApiInfo.host_mtrace + trackingApiInfo.pig_traceability;
+    const qs = {
+        apiKey: trackingApiInfo.api_key,
+        userId: trackingApiInfo.user_id,
+        pigTraceabilityNo: '160074101142'
+    };
+
+    return get(endPoint, qs);
+}
+
+// https://zfhwg0i742.execute-api.ap-northeast-2.amazonaws.com/latest/rest/pig/butchery/pigButcheryInfoDtl
+const getPigButchery = () => {
+    const endPoint = trackingApiInfo.host_mtrace + trackingApiInfo.pig_butchery;
+    const qs = {
+        apiKey: trackingApiInfo.api_key,
+        userId: trackingApiInfo.user_id,
+        pigNo: '160074101142'
+    };
+
+    return get(endPoint, qs);
+};
+
+// https://zfhwg0i742.execute-api.ap-northeast-2.amazonaws.com/latest/openapi-data/service/user/grade/confirm/pig
+const getPigGrade = () => {
+    // const endPoint = trackingApiInfo.host_ekape + trackingApiInfo.pig_grade;
+    const endPoint = trackingApiInfo.host_ekape + trackingApiInfo.pig_grade + `?ServiceKey=${trackingApiInfo.service_key}`;
+    const qs = {
+        issueNo: '0817-10020016',
+        issueDate: '2018-10-02'
+        // ServiceKey: trackingApiInfo.service_key
+    };
+
+    return get(endPoint, qs);
+}
+
+// https://zfhwg0i742.execute-api.ap-northeast-2.amazonaws.com/latest/openapi-data/service/user/grade/confirm/pigPart
+const getPigPartGrade  = () => {
+    // const endPoint = trackingApiInfo.host_ekape + trackingApiInfo.pig_part_grade;
+    const endPoint = trackingApiInfo.host_ekape + trackingApiInfo.pig_part_grade + `?ServiceKey=${trackingApiInfo.service_key}`;
+    const qs = {
+        issueNo: '0817-10020016',
+        issueDate: '2018-10-02'
+        // ServiceKey: trackingApiInfo.service_key
+    };
+
+    return get(endPoint, qs);
+}
+
+// https://zfhwg0i742.execute-api.ap-northeast-2.amazonaws.com/latest/1543000/FarmServiceInfo/getFarmAreaInfo
+const getFarmDetailInfo  = () => {
+    const endPoint = trackingApiInfo.host_data + trackingApiInfo.farm_detail_info + `?serviceKey=${trackingApiInfo.service_key}`;
+    const qs = {
+        pageNo: 1,
+        startPage: 1,
+        numOfRows: 10,
+        pageSize: 10
+        // ServiceKey: trackingApiInfo.service_key
+    };
+
+    return get(endPoint, qs);
+}
+
+// https://zfhwg0i742.execute-api.ap-northeast-2.amazonaws.com/latest/1543000/FarmService/getFarmAreaStatInfo
+const getFarmTotalInfo  = () => {
+    const endPoint = trackingApiInfo.host_data + trackingApiInfo.farm_total_info + `?serviceKey=${trackingApiInfo.service_key}`;
+    const qs = {
+        pageNo: 1,
+        startPage: 1,
+        numOfRows: 10,
+        pageSize: 10
+        // ServiceKey: trackingApiInfo.service_key
+    };
+
+    return get(endPoint, qs);
+}
+
+const get = (endPoint, qs) => new Promise((resolve, reject) => {
+    request.get({
+        url: endPoint,
+        qs: qs
+    }, (err, res, result) => (res.statusCode === 200) ? resolve(parser.parse(result)) :
+        (isUndefined(err) ? reject(JSON.parse(result)) : reject(err)));
+});
+
+const post = (endPoint, header, body) => new Promise((resolve, reject) => {
+    request.post({
+        url: endPoint,
+        headers: header,
+        body: JSON.stringify(body)
+    }, (err, res, result) => (res.statusCode === 200) ? resolve(JSON.parse(result)) :
+        (isUndefined(err) ? reject(JSON.parse(result)) : reject(err)));
+});
+
+module.exports = {
+    getPigTraceability, getPigButchery,
+    getPigGrade, getPigPartGrade,
+    getFarmDetailInfo, getFarmTotalInfo
+}
